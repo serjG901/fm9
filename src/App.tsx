d6a53771f933317eab9ace14eab736968411b9d1
serjG901 <70537823+serjG901@users.noreply.github.com>
2024-10-08 13:33:41 +0300

    clear update fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,16 @@
-//import { ReactNode, useState } from "react";
+import { ReactNode, useState } from "react";
 import "./App.css";
-/*import Menu from "./ui/molecul/menu/Menu";
+import Menu from "./ui/molecul/menu/Menu";
 import Ui from "./pages/ui/Ui";
 import Debets from "./pages/debets/Debets";
 import Credits from "./pages/credits/Credits";
 import Buys from "./pages/buys/Buys";
-import Pays from "./pages/pays/Pays";*/
-import { useBuysStore } from "./store/buysStore";
-import { usePaysStore } from "./store/paysStore";
-import ActionButton from "./ui/atom/action-button/ActionButton";
+import Pays from "./pages/pays/Pays";
 //import Transactions from "./pages/transactions/Transactions";
 
 function App() {
- // const [page, setPage] = useState<string>("buys");
+  const [page, setPage] = useState<string>("buys");
 
-  const [updateFromForB] = useBuysStore(state => [state.updateFromFor]);
-  const [updateFromForP] = usePaysStore(state => [state.updateFromFor]);
-/*
   const pages: { [key: string]: ReactNode } = {
     buys: <Buys />,
     pays: <Pays />,
@@ -28,21 +22,17 @@ function App() {
 
   const handleActionMenu = (payload: string) => {
     setPage(payload);
-  };*/
-  const update = () => {
-    updateFromForB();
-    updateFromForP();
-  }
-/*  <Menu
+  };
+
+  return (
+    <>
+      <Menu
         collapseLevel='menu'
         title='pages'
         options={Object.keys(pages)}
         actionWithPayload={handleActionMenu}
       />
-      {pages[page] || null }*/
-  return (
-    <><ActionButton actionWithPayload={update}>update</ActionButton>
-    
+      {pages[page] || null}
     </>
   );
 }
