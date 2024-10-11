028ab2752dc23ce1b397e0e95594703f766bbf52
serjG901 <70537823+serjG901@users.noreply.github.com>
2024-10-11 16:47:04 +0300

    fix toTop

--- src/ui/molecul/to-top/ToTop.tsx
+++ src/ui/molecul/to-top/ToTop.tsx
@@ -7,7 +7,7 @@ export default function ToTop() {
   };
   return (
     <div className='to-top'>
-      <ActionButton actionWithPayload={toTop}>{"<"}</ActionButton>
+      <ActionButton actionWithPayload={toTop}>{"/\\"}</ActionButton>
     </div>
   );
 }
