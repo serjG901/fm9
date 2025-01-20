b7feb5a9188ac60acffb4c4f0c8c670c5fbac395
serjG901 <70537823+serjG901@users.noreply.github.com>
2025-01-21 00:01:28 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,68 +10,6 @@ export default defineConfig({
       registerType: "autoUpdate",
       injectRegister: false,
 
-      manifest: {
-        short_name: "FM9",
-        name: "FM9: intuitive self financial management",
-        icons: [
-          {
-            src: "/fm9/images/icons-vector-white.svg",
-            type: "image/svg+xml",
-            sizes: "512x512",
-          },
-          {
-            src: "/fm9/images/icons-vector-black.svg",
-            type: "image/svg+xml",
-            sizes: "512x512",
-          },
-          {
-            src: "/fm9/images/apple-touch-icon.png",
-            type: "image/png",
-            sizes: "180x180",
-            purpose: "maskable",
-          },
-          {
-            src: "/fm9/images/icons-192.png",
-            type: "image/png",
-            sizes: "192x192",
-            purpose: "maskable",
-          },
-          {
-            src: "/fm9/images/icons-512.png",
-            type: "image/png",
-            sizes: "512x512",
-            purpose: "maskable",
-          },
-        ],
-        start_url: "/fm9",
-        background_color: "#008080",
-        display: "standalone",
-        scope: "/fm9",
-        description: "Intuitive self financial management",
-        screenshots: [
-          {
-            src: "/fm9/images/screenshot1.png",
-            type: "image/png",
-            sizes: "540x720",
-            form_factor: "narrow",
-          },
-          {
-            src: "/fm9/images/screenshot2.png",
-            type: "image/png",
-            sizes: "720x540",
-            form_factor: "wide",
-          },
-        ],
-        file_handlers: [
-          {
-            action: "/fm9/settings",
-            accept: {
-              "application/json": [".json"],
-            },
-          },
-        ],
-      },
-
       workbox: {
         globPatterns: ["**/*.{js,css,html,svg,png,ico,json}"],
         cleanupOutdatedCaches: true,
