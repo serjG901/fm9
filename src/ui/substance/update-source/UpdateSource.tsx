8595cfa6fb3addea9277c346bcf7fe74cd4b57ba
serjG901 <70537823+serjG901@users.noreply.github.com>
2025-04-12 02:50:54 +0300

    Update UpdateSource.tsx

--- src/ui/substance/update-source/UpdateSource.tsx
+++ src/ui/substance/update-source/UpdateSource.tsx
@@ -13,6 +13,8 @@ interface UpdateSourceComponent extends TextesByLanguage {
   sources?: Source[];
   currencies?: string[];
   defaultHue?: string;
+  isShow?: boolean;
+  setIsShow?: (isShow: boolean) => void;
 }
 
 export default function UpdateSource({
@@ -23,32 +25,56 @@ export default function UpdateSource({
   sources = [],
   currencies = [],
   defaultHue = "",
+  isShow = false,
+  setIsShow = () => {},
 }: UpdateSourceComponent) {
+  const [hueSelf, setHueSelf] = useState(
+    source.hueAsDefault ? defaultHue : source.hue
+  );
+
   const updateSourceAndCloseModal = (s: Source) => {
     updateSource(s);
     document.getElementById(`update-source-${source.id}`)?.hidePopover();
+    setHueSelf(defaultHue);
+    setIsShow(false);
   };
-  const [hueSelf, setHueSelf] = useState(source.hue || defaultHue);
+
+  const hideModal = () => {
+    const modalId = document.getElementById(`update-source-${source.id}`);
+    modalId?.hidePopover();
+    setHueSelf(defaultHue);
+    setIsShow(false);
+  };
+
   return (
     <Contents style={{ "--hue-self": hueSelf } as React.CSSProperties}>
-      <Modal id={`update-source-${source.id}`} textes={textes}>
-        <FormSource
-          textes={textes}
-          actionType='update'
-          actionSource={updateSourceAndCloseModal}
-          source={source}
-          deleteSource={deleteSource}
-          sources={sources}
-          currencies={currencies}
-          defaultHue={hueSelf}
-          setHueSelf={setHueSelf}
-        />
+      <Modal
+        id={`update-source-${source.id}`}
+        textes={textes}
+        hideModal={hideModal}
+      >
+        {isShow ? (
+          <FormSource
+            textes={textes}
+            actionType='update'
+            actionSource={updateSourceAndCloseModal}
+            source={source}
+            deleteSource={deleteSource}
+            sources={sources}
+            currencies={currencies}
+            defaultHue={defaultHue}
+            setHueSelf={setHueSelf}
+          />
+        ) : null}
       </Modal>
       {source.id === 0 ? (
         <ActionButton
-          actionWithPayload={() =>
-            document.getElementById(`update-source-${source.id}`)?.showPopover()
-          }
+          actionWithPayload={() => {
+            document
+              .getElementById(`update-source-${source.id}`)
+              ?.showPopover();
+            setIsShow(true);
+          }}
         >
           show modal
         </ActionButton>
